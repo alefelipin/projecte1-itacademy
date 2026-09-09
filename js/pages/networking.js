c3ee8c55b1631d5cfb2831b5d08f5a57342f9335
AlejandroFelipe Acuna <afacunam@gmail.com>
2026-09-10 00:25:39 +0200

    refactor(networking): improve search filter

--- js/pages/networking.js
+++ js/pages/networking.js
@@ -1,31 +1,14 @@
 "use strict"
 
 const mobileSearchInput = document.getElementById("mobile-search-alumni");
-const mobileSearchAlumni = mobileSearchInput.value.toUpperCase();
-
-const desktopSearchInput = document.getElementById("desktop-search-alumni")
-const desktopSearchAlumni = desktopSearchInput.value.toUpperCase();
+const desktopSearchInput = document.getElementById("desktop-search-alumni");
 
 const alumniCards = document.getElementsByClassName("networking-card");
 
-desktopSearchInput.addEventListener("input", function() {
-
-  for (let i = 0; i < cardText.length; i++ ) {
-
-    const cardText = alumniCards[i].textContent.toUpperCase();
-
-    console.log(cardText);
-
-    if ( cardText.includes(desktopSearchInput) ) {
-      alumniCards[i].style.display = "";
-    } else {
-      alumniCards[i].style.display = "none";
-    }
-  }
-
-});
 
 mobileSearchInput.addEventListener("input", function () {
+
+  const mobileSearchAlumni = mobileSearchInput.value.toUpperCase();
 
   for (let i = 0; i < alumniCards.length; i++) {
 
@@ -41,6 +24,27 @@ mobileSearchInput.addEventListener("input", function () {
   }
 
 });
+
+desktopSearchInput.addEventListener("input", function() {
+
+  const desktopSearchAlumni = desktopSearchInput.value.toUpperCase();
+
+  for (let i = 0; i < alumniCards.length; i++ ) {
+
+    const cardText = alumniCards[i].textContent.toUpperCase();
+
+    if ( desktopSearchAlumni == "") {
+      alumniCards[i].style.display = "";
+    } else if ( cardText.includes(desktopSearchAlumni) ) {
+      alumniCards[i].style.display = "flex";
+    } else {
+      alumniCards[i].style.display = "none";
+    }
+  }
+
+});
+
+
 
 
 
