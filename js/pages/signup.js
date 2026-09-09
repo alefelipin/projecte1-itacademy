6ed4f42bbe7c7796595a9919c13ef7cde06ee5d0
AlejandroFelipe Acuna <afacunam@gmail.com>
2026-09-09 23:00:06 +0200

    feat(signup): implement basic form validation

--- js/pages/signup.js
+++ js/pages/signup.js
@@ -1,3 +1,32 @@
 "use strict"
 
-function 
+function signUpAccount() {
+
+  const nameInput = document.getElementById("name-input").value;
+  const lastNameInput = document.getElementById("lastName-input").value;
+  const emailInput = document.getElementById("email-input").value;
+  const passwordInput = document.getElementById("password-input").value;
+
+  if (nameInput == "") {
+    alert("Introdueix un nom vàlid");
+    return;
+  }
+
+  if (lastNameInput == "") {
+    alert("Introdueix un cognom vàlid");
+    return;
+  }
+
+  if (emailInput == "") {
+    alert("Introdueix un correu electrònic vàlid");
+    return;
+  }
+
+  if (passwordInput != 1234 || passwordInput == "" ) {
+    alert("Contrasenya incorrecta");
+    return;
+  }
+
+  window.location.href = "./home.html";
+
+}
