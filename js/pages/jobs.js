424d02016e0a64398428babe189ab23f1041d0e8
AlejandroFelipe Acuna <afacunam@gmail.com>
2026-09-10 04:35:37 +0200

    refactor(job-opportunities): merge filter functions

--- js/pages/jobs.js
+++ js/pages/jobs.js
@@ -1,32 +1,48 @@
 "use strict"
 
-const industrySelect = document.getElementById("select-industry");
-const experienceSelect = document.getElementById("select-experience");
+const industrySelectMobile = document.getElementById("select-industry-mobile");
+const experienceSelectMobile = document.getElementById("select-experience-mobile");
+
+const industrySelectDesktop = document.getElementById("select-industry-desktop");
+const experienceSelectDesktop = document.getElementById("select-experience-desktop");
 
 const alumniCardsMobile = document.getElementsByClassName("job-opportunities-card");
 const alumniCardsDesktop = document.getElementsByClassName("job-opportunities-desktop-card");
 
-function filterJobsByIndustry() {
 
-  const industrySearchText = industrySelect.value.toUpperCase();
+function filterJobs() {
+
+  const industrySearchTextM = industrySelectMobile.value.toUpperCase();
+  const experienceSearchTextM = experienceSelectMobile.value.toUpperCase();
+  const industrySearchTextD = industrySelectDesktop.value.toUpperCase();
+    const experienceSearchTextD = experienceSelectDesktop.value.toUpperCase();
 
   for (let i = 0; i < alumniCardsMobile.length; i++) {
 
     const cardTextMobile = alumniCardsMobile[i].textContent.toUpperCase();
 
-    if ( cardTextMobile.includes(industrySearchText) ) {
+    const industryCheck = cardTextMobile.includes(industrySearchTextM);
+    const experienceCheck = cardTextMobile.includes(experienceSearchTextM);
+
+    console.log(cardTextMobile);
+
+    if (industryCheck && experienceCheck) {
       alumniCardsMobile[i].style.display = "";
     } else {
       alumniCardsMobile[i].style.display = "none";
     }
 
-  }
+  } 
 
   for (let j = 0; j < alumniCardsDesktop.length; j++) {
 
+
     const cardTextDesktop = alumniCardsDesktop[j].textContent.toUpperCase();
 
-    if ( cardTextDesktop.includes(industrySearchText) ) {
+    const industryCheck = cardTextDesktop.includes(industrySearchTextD);
+    const experienceCheck = cardTextDesktop.includes(experienceSearchTextD);
+
+    if (industryCheck && experienceCheck) {
       alumniCardsDesktop[j].style.display = "";
     } else {
       alumniCardsDesktop[j].style.display = "none";
@@ -36,36 +52,9 @@ function filterJobsByIndustry() {
 
 }
 
-function filterJobsByExperience() {
+industrySelectMobile.addEventListener("change", filterJobs);
+experienceSelectMobile.addEventListener("change", filterJobs);
 
-  const experienceSearchText = experienceSelect.value.toUpperCase();
-
-  for (let i = 0; i < alumniCardsMobile.length; i++) {
-
-    const cardTextMobile = alumniCardsMobile[i].textContent.toUpperCase();
-
-    if ( cardTextMobile.includes(experienceSearchText) ) {
-      alumniCardsMobile[i].style.display = "";
-    } else {
-      alumniCardsMobile[i].style.display = "none";
-    }
-
-  }
-
-  for (let j = 0; j < alumniCardsDesktop.length; j++) {
-
-    const cardTextDesktop = alumniCardsDesktop[j].textContent.toUpperCase();
-
-    if ( cardTextDesktop.includes(experienceSearchText) ) {
-      alumniCardsDesktop[j].style.display = "";
-    } else {
-      alumniCardsDesktop[j].style.display = "none";
-    }
-
-  }
-
-}
-
-industrySelect.addEventListener("change", filterJobsByIndustry);
-experienceSelect.addEventListener("change", filterJobsByExperience);
+industrySelectDesktop.addEventListener("change", filterJobs);
+experienceSelectDesktop.addEventListener("change", filterJobs);
 
